b7ccd87afdb19dfa99a03a80a60be5d47eb710e2
AnhQNguyen <aqnguyen1206@gmail.com>
2021-04-22 15:19:56 -0700

    update lab5 part1

--- turnin/anguy589_lab5_part1.c
+++ turnin/anguy589_lab5_part1.c
@@ -20,11 +20,11 @@ int main(void) {
 
   //temporary variables
   unsigned char tmpA = 0x00;
-  unsigned char tmpC = 0x00;
+//  unsigned char tmpC = 0x00;
 
 
   while(1) {
-    tmpA = PINA & 0x0F;
+    tmpA = ~PINA & 0x0F;
     
       if((tmpA >= 0x01 && tmpA <= 0x02)) {
           PORTC = 0x60; //C5 and C6
@@ -42,7 +42,7 @@ int main(void) {
       else if(tmpA >= 0x0A && tmpA <= 0x0C) {
         PORTC = 0x3E; //C5C4C3C2C1
       }
-      else if(tmpA >= 0x0D && tmpA < 0x0F) {
+      else if(tmpA >= 0x0D && tmpA <= 0x0F) {
         PORTC = 0x3F; //C5C4C3C2C1C0
       }
     else {
